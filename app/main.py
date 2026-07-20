0b7dcbe2a27e0777353e22759a4931dd3dc616f1
Ryot7m <107174339+Ryot7m@users.noreply.github.com>
2026-07-20 18:30:35 +0900

    API起動ファイル追加

---
+++ app/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from app.api import router
+
+from app.config import (
+    API_TITLE,
+    API_VERSION
+)
+
+app = FastAPI(
+    title=API_TITLE,
+    version=API_VERSION,
+)
+
+app.include_router(router)
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
